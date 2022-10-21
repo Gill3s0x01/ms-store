ed66412fc203a0b3f486c329dd6c4eb01b544b5f
Gilles30 <lorison.gilles@proton.me>
2022-10-20 22:22:31 -0300

    feat: creating header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import { HeaderContainer } from './styles'
+import { FaShoppingCart } from 'react-icons/fa'
+
+export const Header = () => {
+  return (
+    <HeaderContainer>
+      <FaShoppingCart />
+    </HeaderContainer>
+  )
+}
